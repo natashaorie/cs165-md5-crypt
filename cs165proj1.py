638bf87312d862707e0f1a21f8be3757b20373ad
natashaorie <norie001@ucr.edu>
2019-01-22 10:03:49 -0800

    adding files

---
+++ cs165proj1.py
@@ -0,0 +1,83 @@
+from hashlib import md5
+import string
+# import thread
+
+# letters = string.ascii_lowercase
+salt = 'hfT7jp2q'
+passHash = 'Csc6Kyx43efmL0sIdsin7'
+passwd = 'abcdef'
+magic = '$1$'
+base64 = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+altSum = md5((passwd + salt + passwd).encode())
+print("The byte equivalent of hash is : ", end ="") 
+print(altSum.hexdigest())
+
+print(altSum.digest_size)
+t = passwd + magic + salt
+if(altSum.digest_size > len(passwd)):
+	t = passwd + magic + salt + str((altSum.digest()[:len(passwd)]))
+
+else:
+	print("passwd longer")
+	i = 0
+	temp = altSum
+	while i < len(passwd):
+		pass
+	t = passwd + magic + salt + passwd[:altSum.digest_size]
+
+print("{0:b}".format(len(passwd)))
+sig = 0
+length = "{0:b}".format(len(passwd))
+length = length[::-1]
+for i, c in enumerate(length):
+	if(c == '1'):
+		t = t + passwd[i]
+		sig = 1
+	else:
+		# if(sig == 1):
+		# 	break
+		t = t + '0'
+
+print(t)	
+intSum = md5(t.encode())
+print(intSum.hexdigest())
+
+i = 0
+while(i < 1000):
+	t = ''
+
+	if(i % 2 == 0):
+		# t += intSum
+		pass
+		
+	if(i % 2 != 0):
+		t += passwd
+
+	if(i % 3 != 0):
+		t += salt
+
+	if(i % 7 != 0):
+		t += passwd
+
+	if(i % 2 == 0):
+		t += passwd
+
+	if(i % 2 != 0):
+		# add intSum
+		pass
+
+	intSum = md5(t.encode())
+	i += 1
+
+
+# for l in letters:
+
+# result = hashlib.md5(b'GeeksforGeeks') 
+# result = md5(b'GeeksforGeeks')
+ 
+
+# printing the equivalent byte value. 
+# print("The byte equivalent of hash is : ", end ="") 
+# print(result.digest())
+# print(result.hexdigest())
