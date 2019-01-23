7f3c5bd9bd12e1be4cc50f66341a1b17a88f82e9
natashaorie <norie001@ucr.edu>
2019-01-22 19:17:46 -0800

    finished encoding to base64

--- cs165proj1.py
+++ cs165proj1.py
@@ -2,7 +2,6 @@ from hashlib import md5
 import string
 # import thread
 
-# letters = string.ascii_lowercase
 salt = 'hfT7jp2q'
 passHash = 'Csc6Kyx43efmL0sIdsin7'
 passwd = 'abcdef'
@@ -10,53 +9,15 @@ magic = '$1$'
 base64 = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
 altSum = md5((passwd + salt + passwd).encode())
-# altSum = md5()
-# altSum.update(passwd)
-# altSum.update(salt)
-# altSum.update(passwd)
 
-print("The byte equivalent of hash is : ", end ="")
-print(altSum.digest())
-
-# print(altSum.digest_size)
-# t = passwd + magic + salt
 intSum = md5((passwd + magic + salt).encode())
+
 if(altSum.digest_size > len(passwd)):
-	# t = passwd + magic + salt + str((altSum.digest()[:len(passwd)]))
-	# temp = altSum.digest()
-	# print(temp[0])
 	intSum.update((altSum.digest()[:len(passwd)]))
-	# intSum.update((altSum.hexdigest()[:len(passwd)*2]).encode())
 
-else:
-	print("passwd longer")
-	i = 0
-	temp = altSum
-	while i < len(passwd):
-		pass
-	t = passwd + magic + salt + passwd[:altSum.digest_size]
-
-print(intSum.digest())
-print(intSum.hexdigest())
-
-# for c in str(intSum.digest()):
-# 	print("{:02x}".format(ord(c)))
-
-print("{0:b}".format(len(passwd)))
-sig = 0
+# sig = 0
 length = "{0:b}".format(len(passwd))
 length = length[::-1]
-
-# for i, c in enumerate(length):
-# 	if(c == '0'):
-# 		# t = t + passwd[i]
-# 		intSum.update(passwd[i].encode())
-# 		sig = 1
-# 	else:
-# 		# if(sig == 1):
-# 		# 	break
-# 		# t = t + '0'
-# 		intSum.update('x00'.encode())
 
 for c in length:
 	if(c == '0'):
@@ -65,22 +26,12 @@ for c in length:
 	else:
 		intSum.update(b'\x00')
 
-# print(t)
-# intSum = md5(t.encode())
-print(intSum.digest())
-print(intSum.hexdigest())
-# for c in str(intSum.digest()):
-# 	print("{:02x}".format(ord(c)))
-
 
 i = 0
-
 currIntSum = intSum
 nextIntSum = md5()
-while(i < 1000):
-	# print(currIntSum.hexdigest())
-	# t = ''
 
+while(i < 1000):
 	if(i % 2 == 0):
 		nextIntSum.update(currIntSum.digest())
 
@@ -101,39 +52,48 @@ while(i < 1000):
 
 	currIntSum = nextIntSum
 	nextIntSum = md5()
-
-	# intSum = md5(t.encode())
 	i += 1
 
 
-print(currIntSum.digest())
-print(currIntSum.hexdigest())
-
-	
-# print("{0:b}".format(currIntSum.hexdigest()))
-# length = "{0:b}".format(currIntSum.hex digest())
-# length = length[::-1]
+i = 0
+reorder = [11, 4, 10, 5, 3, 9, 15, 2, 8, 14, 1, 7, 13, 0, 6, 12]
+reordIntSum = []
 
 
-c = 1
-convertNum;
-asciiNum;
-for i in currIntSum:
-	if c == 1:
-		convertNum = i
-		c = 2
-	else:
-		convertNum += i
-		asciiNum = convertNum->asciiNum
-		c = 1
+while(i < 16): # this gets the bytes into ascii i think
+	binary = "{0:b}".format(currIntSum.digest()[reorder[i]])
+	leastSigBits = 8 - len(binary)
+	reordIntSum.append('0'*leastSigBits + binary)
 
-# for l in letters:
-
-# result = hashlib.md5(b'GeeksforGeeks')
-# result = md5(b'GeeksforGeeks')
+	i += 1
 
 
-# printing the equivalent byte value.
-# print("The byte equivalent of hash is : ", end ="")
-# print(result.digest())
-# print(result.hexdigest())
+reordIntSum = ''.join(reordIntSum)
+# print(len(reordIntSum))
+
+regroup = []
+i = 0
+j = 2
+regroup.append(reordIntSum[i:j])
+i = 2
+j = 8
+while(j <= 128):
+	regroup.append(reordIntSum[i:j])
+	i += 6
+	j += 6
+
+
+regroup = regroup[::-1]
+
+for i, r in enumerate(regroup):
+	regroup[i] = int(r,2)
+
+# print(regroup)
+
+encoding = ''
+for r in regroup:
+	encoding += base64[r]
+
+print(encoding)
+
+
